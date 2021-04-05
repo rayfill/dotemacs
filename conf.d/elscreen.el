8b6ba02330e4cda1666d82097cca3311d83adb25
rayfill <rayfill@gmail.com>
2021-04-05 10:41:53 +0900

    elscreen設定追加

---
+++ conf.d/elscreen.el
@@ -0,0 +1,12 @@
+(eval-when (compile load execute)
+  (require 'elscreen)
+  (require 'elscreen-separate-buffer-list)
+  (require 'ido)
+  (require 'use-package))
+
+
+(use-package elscreen
+  :init
+  (elscreen-start)
+  (elscreen-separate-buffer-list-mode)
+  (ido-mode))
