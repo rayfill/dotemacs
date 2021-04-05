d1071b03fac1b8f43f981b15718dfd7c2180ec07
rayfill <rayfill@gmail.com>
2021-04-05 19:23:36 +0900

    package double initialize fixed

--- package.el
+++ package.el
@@ -1,5 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
+(unless package--initialized
+  (package-initialize))
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
