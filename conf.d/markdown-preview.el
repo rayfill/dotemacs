a21686aca04a02473f1123335c2c239196f8ef8e
rayfill <rayfill@gmail.com>
2021-07-15 15:05:44 +0900

    fix stylesheet settings for markdown preview mode

--- conf.d/markdown-preview.el
+++ conf.d/markdown-preview.el
@@ -3,7 +3,7 @@
   (require 'markdown-preview-mode))
 
 (use-package markdown-preview-mode
-  :custom
-  (markdown-preview-stylesheets (list "https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css"))
+  :config
+  (setq markdown-preview-stylesheets (list "https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css"))
   :hook
   markdown-mode)
